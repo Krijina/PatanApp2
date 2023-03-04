8d7f1c4b00311babad20e203b415d8f5ea496684
krijina <saloni.shakya12340@gmail.com>
2023-03-04 12:40:42 +0545

    Initial commit

---
+++ app/src/main/java/com/example/patanapp/CourseRegister.kt
@@ -0,0 +1,33 @@
+package com.example.patanapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class CourseRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_register);
+//        Button btn = (Button) findViewById(R.id.savebtn);
+//        Button btn1=(Button) findViewById(R.id.cancelbtn);
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent;
+//                intent = new Intent(getApplicationContext(), course.class);
+//                startActivity(intent);
+//            }
+//        });
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent;
+//                intent = new Intent(getApplicationContext(), coursedetail.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
---
+++ app/src/main/java/com/example/patanapp/ui/newsblog/NewsblogFragment.kt
@@ -0,0 +1,42 @@
+package com.example.patanapp.ui.newsblog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.patanapp.databinding.FragmentNewsblogBinding
+
+class NewsblogFragment : Fragment() {
+
+    private var _binding: FragmentNewsblogBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(NewsBlogModel::class.java)
+
+        _binding = FragmentNewsblogBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
